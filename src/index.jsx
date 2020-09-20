0b196b0af91d922652730cb1ea5e3b264210985c
andrijunaedi <andrijunaedi126@gmail.com>
2020-09-20 09:05:48 +0700

    integration firebase

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './containers/pages/App';
 import * as serviceWorker from './serviceWorker';
+import firebase from './config/firebase';
 
+console.log(firebase);
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want your app to work offline and load faster, you can change
